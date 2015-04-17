27f605a154f7b2f37e7eb6a95927b88c27607656
Luke Shillabeer <lshillabeer@gmail.com>
2015-04-17 13:38:29 +1000

    Altered way trip/location/place are AJAX'ed to fix bug - Assures that a trip will exist for ALL place creations

--- public/js/splash.js
+++ public/js/splash.js
@@ -2,10 +2,10 @@ var trip = {
   places: [],
   trip_id: -1,
   trip_name: undefined,
-  createTrip: function() {
+  storePlace: function(loc) {
+    // Store a place (along with location) and associate 
+    // it with the current 'trip'
     
-    // POST call to /api/trip to generate trip
-    // conditional on trip not currently having a trip_id set (unset = -1)
     if (trip.trip_id === -1) {
       constructed_trip = $.ajax({
         url: '/api/trip',
@@ -13,51 +13,51 @@ var trip = {
         data: {},
         success: function(data, textStatus, jqXHR) {
           trip.trip_id = data.id;
-          console.log('trip success')
+          console.log('trip success');
+          place_and_loc_create(loc);
+        }
+      });
+    } else {
+      place_and_loc_create(loc);
+    } 
+  }
+}
+
+function place_and_loc_create(loc) {
+  $.ajax({
+    url: "/api/place",
+    type: "POST",
+    data: {
+      trip_id: trip.trip_id,
+    },
+    success: function(data, textStatus, jqXHR) {
+      trip.places.push(data.id);
+      console.log('place success');
+      contructed_location = $.ajax({
+        url: "/api/location",
+        type: "POST",
+        data: {
+          latitude: loc.latitude,
+          longitude: loc.longitude,
+          street_number: loc.street_number,
+          route: loc.route,
+          locality: loc.locality,
+          admin_area: loc.administrative_area_level_1,
+          country: loc.country,
+          post_code: loc.postal_code,
+          full_address: loc.full_address,
+          place_id: data.id
+        },
+        success: function(data, textStatus, jqXHR) {
+          console.log('location success');
+          places_obj.resetLocation();
         }
       });
     }
-  },
-  storePlace: function(loc) {
-    // Store a place (along with location) and associate 
-    // it with the current 'trip'
-    
-    constructed_place = $.ajax({
-      url: "/api/place",
-      type: "POST",
-      data: {
-        trip_id: trip.trip_id,
-      },
-      success: function(data, textStatus, jqXHR) {
-        trip.places.push(data.id);
-        console.log('place success');
-        contructed_location = $.ajax({
-          url: "/api/location",
-          type: "POST",
-          data: {
-            latitude: loc.latitude,
-            longitude: loc.longitude,
-            street_number: loc.street_number,
-            route: loc.route,
-            locality: loc.locality,
-            admin_area: loc.administrative_area_level_1,
-            country: loc.country,
-            post_code: loc.postal_code,
-            full_address: loc.full_address,
-            place_id: data.id
-          },
-          success: function(data, textStatus, jqXHR) {
-            console.log('location success');
-            places_obj.resetLocation();
-          }
-        });
-      }
-    });
-  },
+  });
 }
 
 function loc_add_btn_press() { 
-  trip_created = trip.createTrip();
   
   $("#mobile-content-container").fadeIn("slow");
   $("#mobile-content-button").fadeIn("slow");
@@ -69,7 +69,7 @@ function loc_add_btn_press() {
   // TODO: implement this with a javascript "promise"
   setTimeout(function(){ 
     trip.storePlace(places_obj); 
-  }, 500);
+  }, 1000);
 
   var $autocomp = $('#autocomplete');
   $('#mobile-content-container ul').append("<li>" + $autocomp.val());
