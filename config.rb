571406b42590f8ffe4f1531dab3b1eda6256aa22
Luke Shillabeer <lshillabeer@gmail.com>
2015-04-17 12:37:38 +1000

    Edited db connection for Heroku rack deployment - Now deals with deployment AND local cast (and doesn't break)

--- config.rb
+++ config.rb
@@ -1,4 +1,3 @@
-ActiveRecord::Base.establish_connection({
-  :adapter => 'postgresql',
-  :database => 'HEROKU_POSTGRESQL_WHITE_URL'
-})
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(ENV['HEROKU_POSTGRESQL_WHITE_URL'] || 'postgres://localhost/travellr')
